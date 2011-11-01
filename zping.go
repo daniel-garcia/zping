9445df91f76e5ba0f455a934ad7c0f59ae6de41e
Daniel Garcia <daniel@danielgarcia.info>
2011-11-01 02:24:08 -0500

    first pass at converting handler to go routine

--- zping.go
+++ zping.go
@@ -54,74 +54,121 @@ func makePingRequest(id, seq, pktlen int, filler []byte) []byte {
 	return p
 }
 
+
+type PingResponse struct {
+	dsthost string
+	rtt int64
+}
+
+type PingRequest struct {
+	dsthost string
+	responseChannel chan PingResponse
+}
+
+
 func parsePingReply(p []byte) (id, seq int) {
 	id = int(p[4])<<8 | int(p[5])
 	seq = int(p[6])<<8 | int(p[7])
 	return
 }
 
+
+func PingPoller (service chan PingRequest) {
+
+	srchost := ""
+	for {
+		r := <- service
+		log.Printf("Received request for %s", r.dsthost)
+		var (
+			laddr *net.IPAddr
+			err   os.Error
+		)
+
+		raddr, err := net.ResolveIPAddr(r.dsthost)
+		if err != nil {
+			log.Println(`net.ResolveIPAddr("%v") = %v, %v`, r.dsthost, raddr, err)
+			reply := PingResponse{r.dsthost, -1}
+			r.responseChannel <- reply
+			continue
+		}
+
+		c, err := net.ListenIP("ip4:icmp", laddr)
+		if err != nil {
+			log.Println(`net.ListenIP("ip4:icmp", %v) = %v, %v`, srchost, c, err)
+			reply := PingResponse{r.dsthost, -1}
+			r.responseChannel <- reply
+			continue
+		}
+
+		sendid := os.Getpid() & 0xffff
+		const sendseq = 61455
+		const pingpktlen = 128
+		sendpkt := makePingRequest(sendid, sendseq, pingpktlen, []byte("Go Go Gadget Ping!!!"))
+
+		start := time.Nanoseconds()
+		n, err := c.WriteToIP(sendpkt, raddr)
+		if err != nil || n != pingpktlen {
+			log.Println(`net.WriteToIP(..., %v) = %v, %v`, raddr, n, err)
+			reply := PingResponse{r.dsthost, -1}
+			r.responseChannel <- reply
+			continue
+		}
+
+		c.SetTimeout(100e6)
+		resp := make([]byte, 1024)
+		for {
+			n, from, err := c.ReadFrom(resp)
+			if err != nil {
+				log.Println(`ReadFrom(...) = %v, %v, %v`, n, from, err)
+				reply := PingResponse{r.dsthost, -1}
+				r.responseChannel <- reply
+				break
+			}
+			if resp[0] != ICMP_ECHO_REPLY {
+				continue
+			}
+			rcvid, rcvseq := parsePingReply(resp)
+			end := time.Nanoseconds()
+			if rcvid != sendid || rcvseq != sendseq {
+				log.Println(`Ping reply saw id,seq=0x%x,0x%x (expected 0x%x, 0x%x)`, rcvid, rcvseq, sendid, sendseq)
+				reply := PingResponse{r.dsthost, -1}
+				r.responseChannel <- reply
+				break
+			}
+			log.Println("response took %d nanoseconds.", end - start)
+			reply := PingResponse {r.dsthost, end - start}
+			r.responseChannel <- reply
+			break
+		}
+		log.Println("saw no ping return")
+		reply := PingResponse{r.dsthost, -1}
+		r.responseChannel <- reply
+	}
+}
+
 func handler (w http.ResponseWriter, r *http.Request) {
+
+	responseChan := make(chan PingResponse)
+	request := PingRequest { r.URL.Path[1:], responseChan }
+	requestChan <- request
+	var (
+		response PingResponse
+	)
+        response = <- request.responseChannel
+
+	fmt.Fprintln(w, "response took %d nanoseconds.", response.rtt)
+}
+
+var (
+	requestChan chan PingRequest
+)
+
+func main() {
 	if os.Getuid() != 0 {
 		log.Fatal("test disabled; must be root")
 	}
-
-	dsthost := r.URL.Path[1:]
-	srchost := ""
-
-	log.Printf("Received request for %s", dsthost)
-	log.Printf("Received request for %s", srchost)
-	var (
-		laddr *net.IPAddr
-		err   os.Error
-	)
-
-	raddr, err := net.ResolveIPAddr(dsthost)
-	if err != nil {
-		fmt.Fprintln(w, `net.ResolveIPAddr("%v") = %v, %v`, dsthost, raddr, err)
-		return
-	}
-
-	c, err := net.ListenIP("ip4:icmp", laddr)
-	if err != nil {
-		fmt.Fprintln(w, `net.ListenIP("ip4:icmp", %v) = %v, %v`, srchost, c, err)
-	}
-
-	sendid := os.Getpid() & 0xffff
-	const sendseq = 61455
-	const pingpktlen = 128
-	sendpkt := makePingRequest(sendid, sendseq, pingpktlen, []byte("Go Go Gadget Ping!!!"))
-
-	start := time.Nanoseconds()
-	n, err := c.WriteToIP(sendpkt, raddr)
-	if err != nil || n != pingpktlen {
-		fmt.Fprintln(w, `net.WriteToIP(..., %v) = %v, %v`, raddr, n, err)
-		return
-	}
-
-	c.SetTimeout(100e6)
-	resp := make([]byte, 1024)
-	for {
-		n, from, err := c.ReadFrom(resp)
-		if err != nil {
-			fmt.Fprintln(w, `ReadFrom(...) = %v, %v, %v`, n, from, err)
-			return
-		}
-		if resp[0] != ICMP_ECHO_REPLY {
-			continue
-		}
-		rcvid, rcvseq := parsePingReply(resp)
-		end := time.Nanoseconds()
-		if rcvid != sendid || rcvseq != sendseq {
-			fmt.Fprintln(w, `Ping reply saw id,seq=0x%x,0x%x (expected 0x%x, 0x%x)`, rcvid, rcvseq, sendid, sendseq)
-			return
-		}
-		fmt.Fprintln(w, "response took %d nanoseconds.", end - start)
-		return
-	}
-	fmt.Fprintln(w, "saw no ping return")
-}
-
-func main() {
+	requestChan = make(chan PingRequest)
+	go PingPoller(requestChan)
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8081", nil)
 }
